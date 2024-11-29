a25e1c5e3948a388f79bdc27cbe7e6b66f8a556c
user <user@WIN-S3FLAGBG853>
2024-11-29 18:35:02 +0300

    INSERT

--- DML/SQLQuery6 - AGREGATION.sql
+++ DML/SQLQuery6 - AGREGATION.sql
@@ -1,13 +1,19 @@
 USE Acadamy_318_SQL;
 
 SELECT 
---COUNT(direction_id) AS N'Количество'
-		--group_name		AS N'Группа',
 		Directions.direction_name		AS N'Направление обучения',
-		COUNT(direction_id)OVER (PARTITION BY ) AS N'Количество групп', --AND
-		COUNT(Students.student_id)OVER (PARTITION BY ) AS N'Количество студентов'
+		COUNT(Groups.group_id)			AS N'Количество групп'
+FROM Directions, Groups--, Students
+WHERE  Groups.direction = Directions.direction_id --AND [group] = Groups.group_id
+GROUP BY direction_name
+;
 
-FROM Directions, Students, Groups-- --;
+SELECT 
+		Directions.direction_name		AS N'Направление обучения',
+		--COUNT((Groups.direction = Directions.direction_id)=1) AS N'Количество групп',--все равно неверный результат чувствую
+		COUNT(Students.student_id)		AS N'Количество студентов'
+
+FROM Directions, Students, Groups
 WHERE  Groups.direction = Directions.direction_id AND [group] = Groups.group_id
 GROUP BY direction_name
 ;
