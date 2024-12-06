6c6a709e787c38102e01475d6e8e2fccb582a2f3
user <user@WIN-S3FLAGBG853>
2024-12-06 21:47:04 +0300

    sp_MayHolidaysFor

---
+++ DML/SQLQuery8 - INSERT DaysOFF.sql
@@ -0,0 +1,18 @@
+USE PD_318_DML;
+GO
+SET DATEFIRST 1;
+
+--SELECT * FROM Holidays;
+
+--INSERT DaysOFF
+--		([date],holiday)
+--VALUES
+--		(1, N'Новый год'),
+;
+
+--PRINT (DATENAME(WEEKDAY,'2025-01-01'));
+--PRINT (DATENAME(WEEKDAY,'2024-12-31'));
+--PRINT (DATEADD(DAY, DATEPART(WEEKDAY,'2023-12-31')*(-1)+1,'2023-12-31'));
+
+PRINT (dbo.GetLastMonOfYear(2023));
+PRINT (dbo.GetLastMonOfApril(2025));
