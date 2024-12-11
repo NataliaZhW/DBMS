ee0ecef90bd237a78c5bba57776b8bf8773a3e30
user <user@WIN-S3FLAGBG853>
2024-12-11 18:32:36 +0300

    ALTER PROCEDURE sp_SetScheduleForStacionar

--- SQL/Functions/SQLQuery0 - CheckHolidays.sql
+++ SQL/Functions/SQLQuery0 - CheckHolidays.sql
@@ -7,7 +7,7 @@ SET DATEFIRST 1;
 --EXEC sp_SummerHolidaysFor 2024;
 --
 
-EXEC sp_SetDaysOffFor 2025;
+--EXEC sp_SetDaysOffFor 2025;
 
 SELECT 
 [date], 
@@ -16,4 +16,4 @@ holiday_name
 --FROM DaysOFF, Holidays WHERE holidey = holidey_id
 FROM DaysOFF JOIN Holidays ON(holiday = holiday_id)
 ;
---SELECT * From Holidays;
+SELECT * From Holidays;
--- SQL/Schedule/SQLQuery5 - StacionatToSchedule PROCEDURE.sql
+++ SQL/Schedule/SQLQuery5 - StacionatToSchedule PROCEDURE.sql
@@ -1,7 +1,8 @@
 USE PD_318_DML;
 GO
 
-CREATE PROCEDURE sp_SetScheduleForStacionar
+--CREATE PROCEDURE sp_SetScheduleForStacionar
+ALTER PROCEDURE sp_SetScheduleForStacionar
 	@group_name			AS	NVARCHAR(16),
 	@discipline_name	AS	NVARCHAR(150),
 	@teacher_last_name	AS	NVARCHAR(150),
@@ -36,30 +37,41 @@ BEGIN
 
 		PRINT(@number_of_lesson);
 		PRINT(@time);
-		IF NOT EXISTS (SELECT lesson_id FROM Schedule 
-						WHERE [date]=@date AND [time]=@time 
-						AND [group]=@group AND discipline=@discipline)
-		BEGIN
-			INSERT	Schedule
-					([date], [time], [group], discipline, teacher, spent)
-			VALUES	(@date,  @time,	 @group, @discipline, @teacher, IIF(@date < GETDATE(), 1, 0))
-		END
-		SET @number_of_lesson = @number_of_lesson + 1;
-		PRINT('----------------------');
+		--ѕровер€ем, есть ли данна€ дата в таблице выходных
+		IF NOT EXISTS (SELECT [date] FROM DaysOFF 
+						WHERE [date]=@date)
+			BEGIN
+			--ѕровер€ем, есть ли данна€ дата в таблице расписани€, стоит ли там на наше врем€ зан€тие.
+			--Ќе думаю что надо провер€ть группу и дисциплину, так как если врем€ зан€то,
+			--то еще пару другого предмета туда же не надо ставить
+				IF NOT EXISTS (SELECT lesson_id FROM Schedule 
+								WHERE [date]=@date AND [time]=@time 
+								--AND [group]=@group AND discipline=@discipline
+								)
+				BEGIN
+					INSERT	Schedule
+							([date], [time], [group], discipline, teacher, spent)
+					VALUES	(@date,  @time,	 @group, @discipline, @teacher, IIF(@date < GETDATE(), 1, 0))
+					--нумерацию урока надо увеличивать здесь, а не за пределами IF
+					SET @number_of_lesson = @number_of_lesson + 1; --!!
+				END
+				PRINT('----------------------');
 
-		PRINT(@number_of_lesson);
-		PRINT(DATEADD(MINUTE, 90,@time));
-		IF NOT EXISTS (SELECT lesson_id FROM Schedule 
-						WHERE [date]=@date AND [time]=DATEADD(MINUTE, 90, @time) 
-						AND [group]=@group AND discipline=@discipline)
-		BEGIN
-			INSERT	Schedule
-					([date], [time], [group], discipline, teacher, spent)
-			VALUES	(@date, DATEADD(MINUTE, 90, @time), @group, @discipline, @teacher, IIF(@date < GETDATE(), 1, 0))
-		END
+				PRINT(@number_of_lesson);
+				PRINT(DATEADD(MINUTE, 90,@time));
+				IF NOT EXISTS (SELECT lesson_id FROM Schedule 
+								WHERE [date]=@date AND [time]=DATEADD(MINUTE, 90, @time) 
+								--AND [group]=@group AND discipline=@discipline
+								)
+				BEGIN
+					INSERT	Schedule
+							([date], [time], [group], discipline, teacher, spent)
+					VALUES	(@date, DATEADD(MINUTE, 90, @time), @group, @discipline, @teacher, IIF(@date < GETDATE(), 1, 0))
+					SET @number_of_lesson = @number_of_lesson + 1; --!!
+				END
 
-		SET @number_of_lesson = @number_of_lesson + 1;
-		PRINT('======================');
+				PRINT('======================');
+			END
 
 		SET @date = DATEADD(DAY, IIF(DATEPART(DW,@date)=1 OR DATEPART(DW,@date)=3,2,3), @date);
 	END
